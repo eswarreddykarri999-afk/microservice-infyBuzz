db6b45c886f977b82e90ef695230fd118faab15b
eswarreddykarri999-afk <eswarreddykarri999@gmail.com>
2025-10-27 20:00:28 +0530

    first

---
+++ src/main/java/com/ibm/app/StudentService1Application.java
@@ -0,0 +1,13 @@
+package com.ibm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentService1Application.class, args);
+	}
+
+}
